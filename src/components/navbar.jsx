ec4e85a0a93ad9815c5afc0643350a26d2ea2a54
tidzi17 <tidzi15110@gmail.com>
2023-11-30 23:45:14 +0100

    idontknow

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -14,10 +14,10 @@ export default function Navbar(){
     return( 
             <div className="w-11/12 h-20 pt-10 z-20  top-0 mx-auto mb-3 " >
                 <nav className="h-fit w-full border-b-2 border-primary flex items-center justify-between min800:relative">
-                <div className="self-center -ml-56 order-2 min800:order-1 min800:ml-0 min800"><p className="text-5xl text-secondary font-cormorant uppercase cursor-pointer md1000:text-2xl min620:text-3xl" onClick={() => navigate("/")}>Herbify</p></div>
-                <button className="max800:hidden  min800:order-3  min800 right-0 min800:absolute" onClick={() => setMobile(!Mobile)}>
+                <div className="self-center -ml-56 order-2 min800:order-1 min800:ml-0"><p className="text-5xl text-secondary font-cormorant uppercase cursor-pointer md1000:text-2xl min620:text-3xl" onClick={() => navigate("/")}>Herbify</p></div>
+               {/*  <button className="max800:hidden  min800:order-3  min800 right-0 " onClick={() => setMobile(!Mobile)}>
                 {Mobile ? <ImCross className="min800:text-5xl"/> : <FaBars className="min800:text-5xl"/>}
-                </button>
+                </button> */}
                 <ul className={Mobile ? "top-11 z-30 bg-slate-100 h-fit w-full border-b-2 border-black min800:absolute" : "flex items-center justify-between order-1 min800:hidden mr"}  onClick={() => setMobile(false)} >
                 <div className="flex  gap-9 flex-wrap min800:w-full min800:h-full min800:flex-col items-center ">
                 <li className="text-xl cursor-pointer md1000:text-sm min800:p-5 min800:text-xxl " onClick={() => navigate("/")}>Home</li>
@@ -26,9 +26,12 @@ export default function Navbar(){
                 <li className="text-xl text-primaryRed cursor-pointer md1000:text-sm min800:p-5 min800:text-xxl" onClick={() => navigate("/sale")}>Sale</li>
                 </div>
                 </ul>
-                <div className="flex justify-between gap-3 items-center mr-4 order-3 min800:order-2  min800:-mr-72 min620:-mr-36 min450:-mr-10">
-                <MdShoppingCart className="text-2xl cursor-pointer md1000:text-base min620:text-xl  " onClick={() => navigate("/cart")}/> /
-                <FaHeart className="text-2xl text-secondary cursor-pointer  md1000:text-base min620:text-xl  " onClick={() => navigate("/wishlist")}/>
+                <div className="flex justify-between gap-3 items-center mr-4 order-3 min800:order-2 min800:mr-0">
+                <MdShoppingCart className="w-full h-full text-5xl cursor-pointer md1000:text-base min620:text-xl  min800:h-full min800:w-full" onClick={() => navigate("/cart")}/> 
+                <FaHeart className="w-full h-full text-secondary cursor-pointer  md1000:text-base min620:text-xl  min800:h-full min800:w-full" onClick={() => navigate("/wishlist")}/>
+                <button className="max800:hidden   min800:right-0 min800:h-full min800:w-full " onClick={() => setMobile(!Mobile)}>
+                {Mobile ? <ImCross className="min800:h-full min800:w-full"/> : <FaBars className="min800:h-full min800:w-full"/>}
+                </button>
                 </div>
                 </nav>
             </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ import '../index.css';
 export default function HomePage(){
     return(
         <div>
-            <h1 className="font-bold text-3xl ">This is Home Page</h1>
+            <h1 className="font-bold h-96 mt-36 text-3xl ">This is Home Page</h1>
 
         </div>
     )
