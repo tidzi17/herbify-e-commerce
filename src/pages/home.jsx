0a53a467e17ac1d96b3cda25eb9be1053013949b
tidzi17 <tidzi15110@gmail.com>
2023-11-30 19:56:00 +0100

    iii

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,17 +6,6 @@ export default function HomePage(){
     return(
         <div>
             <h1 className="font-bold text-3xl ">This is Home Page</h1>
-            <ul className="flex flex-col bg-black">
-                <li>Somethins</li>
-                <li>Somethins</li>
-                <li>Somethins</li>
-                <li>Somethins</li>
-                <li>Somethins</li>
-                <li>Somethins</li>
-                <li>Somethins</li>
-
-            </ul>
-           
 
         </div>
     )
